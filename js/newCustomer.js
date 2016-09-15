0cbe5796a52f08c6bc184562693a4d79bde4d886
orbiran88 <orbiran@outlook.com>
2016-09-15 16:30:39 +0300

    Multiselect - Ver 3

--- js/newCustomer.js
+++
@@ -1,9 +0,0 @@
-function Try() {
-
-//var elemB = document.getElementById("exampleInputEmail1").value;
-    "use strict";
-    var xmlHttp = new XMLHttpRequest();
-    xmlHttp.open("GET", "http://google.com", false); // false for synchronous request
-    xmlHttp.send(null);
-    window.alert("sometext");
-}
---
+++ js/semanticAddons.js
@@ -0,0 +1,7 @@
+$(document).ready(function(){
+            $('.ui.fluid.multiple.search.selection.dropdown').dropdown({
+            allowAdditions: true
+          });
+        });
+
+$('.tagsInput').fastselect();
